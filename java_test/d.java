fda3af3581b748919c790e9a81580c6db509cf30
Hoyong <ghdyd6625@naver.com>
2025-03-04 02:04:04 +0900

    입부 문제 테스트

--- java_test/d.java
+++ java_test/d.java
@@ -1,9 +1,26 @@
 package java_test;
 
+import java.util.*;
+
 public class d {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num <= 0)
+            System.out.println("X");
+
+        else {
+            int sum = 0;
+            for (int i = 1; i <= num; i++) {
+                sum += i;
+            }
+
+            System.out.println(sum);
+        }
+
+        sc.close();
 
     }
 
--- java_test/f.java
+++ java_test/f.java
@@ -1,11 +1,30 @@
 package java_test;
 
-// 이부분에 class 선언 해도 괜찮습니다
+import java.util.*;
 
 public class f {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++)
+                System.out.printf(" ");
+            for (int j = 0; j < 1 + 2 * i; j++)
+                System.out.printf("*");
+            System.out.println();
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - 1 - i; j++)
+                System.out.printf(" ");
+            for (int j = 0; j < 1 + 2 * i; j++)
+                System.out.printf("*");
+            System.out.println();
+        }
+
+        sc.close();
 
     }
 
