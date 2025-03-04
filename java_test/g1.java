02b485e6af94b37944ec6b4a28dba6137e29b468
Hoyong <ghdyd6625@naver.com>
2025-03-04 14:43:19 +0900

    입부 테스트 문제 풀이

--- java_test/g1.java
+++ java_test/g1.java
@@ -1,12 +1,48 @@
 package java_test;
 
-// 이부분에 class 선언 해도 괜찮습니다
+import java.util.*;
+
+class Fan {
+    private String model;
+    private int price;
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
 
 public class g1 {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner sc = new Scanner(System.in);
+        Fan[] fans = new Fan[3];
+        for (int i = 0; i < 3; i++) {
+            fans[i] = new Fan();
+            fans[i].setModel(sc.nextLine().trim());
+            fans[i].setPrice(sc.nextInt());
+            sc.nextLine();
+        }
 
+        Fan minFan = fans[0];
+        for (int i = 1; i < 3; i++) {
+            if (fans[i].getPrice() < minFan.getPrice())
+                minFan = fans[i];
+        }
+
+        System.out.println(minFan.getModel());
+        sc.close();
     }
 
 }
